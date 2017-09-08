c352696e8251b69b55a9ec8e18b863c1d10e158c
Dylan K. Taylor <odigiman@gmail.com>
2017-09-08 20:04:26 +0100

    Bump version to 1.12.3

--- src/DevTools/ConsoleScript.php
+++ src/DevTools/ConsoleScript.php
@@ -15,7 +15,7 @@
  * GNU General Public License for more details.
 */
 
-const VERSION = "1.12.2";
+const VERSION = "1.12.3";
 
 $opts = getopt("", ["make:", "relative:", "out:", "entry:", "compress", "stub:"]);
 
