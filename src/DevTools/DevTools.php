f4b8d77e2ede4b358342a59ad077fd1efe7525dd
Dylan K. Taylor <odigiman@gmail.com>
2018-04-13 17:27:22 +0100

    DevTools: fixed typo in FilesystemIterator

--- src/DevTools/DevTools.php
+++ src/DevTools/DevTools.php
@@ -272,7 +272,7 @@ class DevTools extends PluginBase implements CommandExecutor{
 			implode('|', $this->preg_quote_array($includedPaths, '/')) //... and must be followed by one of these relative paths, if any were specified. If none, this will produce a null capturing group which will allow anything.
 		);
 
-		$directory = new \RecursiveDirectoryIterator($basePath, \FilesystemIterator::SKIP_DOTS | \FilesystemIterator::FOLLOW_SYMLINKS | \FileSystemIterator::CURRENT_AS_PATHNAME); //can't use fileinfo because of symlinks
+		$directory = new \RecursiveDirectoryIterator($basePath, \FilesystemIterator::SKIP_DOTS | \FilesystemIterator::FOLLOW_SYMLINKS | \FilesystemIterator::CURRENT_AS_PATHNAME); //can't use fileinfo because of symlinks
 		$iterator = new \RecursiveIteratorIterator($directory);
 		$regexIterator = new \RegexIterator($iterator, $regex);
 
