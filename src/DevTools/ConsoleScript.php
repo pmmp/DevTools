674cadadbc48ce090da20ed2f30e16edcf274772
TheNewHEROBRINEX <TheNewHEROBRINEX@users.noreply.github.com>
2021-01-11 19:39:06 +0100

    ConsoleScript: Allow to include single files by passing them via --make (#52)

--- src/DevTools/ConsoleScript.php
+++ src/DevTools/ConsoleScript.php
@@ -55,7 +55,8 @@ function preg_quote_array(array $strings, string $delim = null) : array{
 function buildPhar(string $pharPath, string $basePath, array $includedPaths, array $metadata, string $stub, int $signatureAlgo = \Phar::SHA1, ?int $compression = null){
 	$basePath = rtrim(str_replace("/", DIRECTORY_SEPARATOR, $basePath), DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR;
 	$includedPaths = array_map(function($path) : string{
-		return rtrim(str_replace("/", DIRECTORY_SEPARATOR, $path), DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR;
+		$path = rtrim(str_replace("/", DIRECTORY_SEPARATOR, $path), DIRECTORY_SEPARATOR);
+		return is_dir($path) ? $path . DIRECTORY_SEPARATOR : $path;
 	}, $includedPaths);
 	if(file_exists($pharPath)){
 		yield "Phar file already exists, overwriting...";
