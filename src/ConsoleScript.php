1b264da3d244cd9b0030cbc3d584d2f685648bfe
Dylan K. Taylor <dktapps@pmmp.io>
2022-06-01 19:52:40 +0100

    Bump API version for PM5 development

--- src/ConsoleScript.php
+++ src/ConsoleScript.php
@@ -17,7 +17,7 @@ declare(strict_types=1);
  * GNU General Public License for more details.
 */
 
-const DEVTOOLS_VERSION = "1.15.0";
+const DEVTOOLS_VERSION = "1.16.0";
 
 const DEVTOOLS_REQUIRE_FILE_STUB = '<?php require("phar://" . __FILE__ . "/%s"); __HALT_COMPILER();';
 const DEVTOOLS_PLUGIN_STUB = '
